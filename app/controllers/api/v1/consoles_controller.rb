a1ed740bc0d1035f59d2ca420852f5cbe4596dca
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-26 20:54:21 -0300

    20° commit - tdd controller: Consegue listar todos os consoles e retornar status 200? GREEN

---
+++ app/controllers/api/v1/consoles_controller.rb
@@ -0,0 +1,27 @@
+# classe herda de ApplicationController
+class Api::V1::ConsolesController < ApplicationController
+
+  # função usada antes das actions show, update e destroy
+  before_action :set_console, only: %i[show update destroy]
+
+  # retorna todos os consoles cadastrados em formato json
+  def index
+    @consoles = Console.all 
+    render json: @consoles
+  end
+
+
+# funções privadas
+private
+
+# busca no banco um console com id enviado na requisição
+def set_console
+  @console = Console.find(params[:id])
+end
+
+# permite que os campos name e manufacturer sejam manipulados nas requisições
+def console_params
+  params.require(:console).permit(:name, :manufacturer)
+end
+
+end
