d3e52fe49efd7a91326d0f2205b239b17b945cc7
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-26 21:08:17 -0300

    21° commit - ttd controller: Consegue listar um console especifico e retornar status 200? RED

--- spec/controllers/consoles_controller_v1_spec.rb
+++ spec/controllers/consoles_controller_v1_spec.rb
@@ -8,6 +8,14 @@ RSpec.describe Api::V1::ConsolesController, type: :controller do
     it 'Consegue listar todos os consoles e retornar status 200?' do
       get :index
       expect(JSON.parse(response.body).size).to eq(1)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /api/v1/consoles/id' do
+    it 'Consegue listar um console especifico e retornar status 200?' do
+      get :show, params: {id: @console.id}
+      expect(response.body).to include_json(name: 'snes')
     end
   end
 
